2c97e97dba23d6b9574002f3a99c52bf8870e330
Georg Graf (SPG) <grafg@spengergasse.at>
2024-01-17 16:29:09 +0100

    Änfänge der PLF3

---
+++ 4HWII_PLF3_fetch_post_prisma/index.js
@@ -0,0 +1,7 @@
+userInput = document.getElementById('username');
+passInput = document.getElementById('password');
+
+function submit() {
+    console.log(userInput.value);
+    console.log(passInput.value);
+}
