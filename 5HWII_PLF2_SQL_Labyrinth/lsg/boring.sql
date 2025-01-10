97b67e9ef26d8ace9212a7fa5bf090c26dc04b3e
Georg Graf (SPG) <grafg@spengergasse.at>
2025-01-11 00:12:19 +0100

    Habe fertig

--- 5HWII_PLF2_SQL_Labyrinth/lsg/boring.sql
+++ 5HWII_PLF2_SQL_Labyrinth/lsg/boring.sql
@@ -4,6 +4,16 @@ FROM
     cinema
 WHERE
     description <> 'langweilig'
-    AND (id % 2 = 1)
+    AND (id % 2 == 1)
 ORDER BY
     rating DESC;
+
+SELECT
+    p.personid,
+    p.firstname,
+    p.lastname,
+    city,
+    state
+FROM
+    person p
+    LEFT JOIN address a ON p.personid = a.personid;
